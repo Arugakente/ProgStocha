0e3d1ce0f8f755a81a18548ec9823e6a1a955cd4
Gael67350 <gael67350@gmail.com>
2020-11-10 10:32:05 +0100

    Possibility to not generate the subtours in the subgraph_gen script

--- tools/subgraph_gen.py
+++ tools/subgraph_gen.py
@@ -5,6 +5,14 @@ import os
 import numpy as np
 from classes.point import Point
 
+with_subtours = False
+
+if len(sys.argv) < 2:
+    print("Usage : " + str(sys.argv[0]) + " <input_filename> <with_subtours>")
+    sys.exit(1)
+elif len(sys.argv) == 3 and int(sys.argv[2]) == 1:
+    with_subtours = True
+
 ifile = open("../data/rawData/" + sys.argv[1] + ".tsp", "r")
 
 dim = 0
@@ -13,6 +21,7 @@ labels = []
 all_path = []
 is_data_section = False
 
+print("Parsing file...")
 for line in ifile:
 	if 'NODE_COORD_SECTION' in line:
 		is_data_section = True
@@ -30,6 +39,7 @@ for line in ifile:
 
 ifile.close()
 
+print("Generating distance matrix...")
 matrix = np.empty([dim,dim])
 
 for i in range(dim):
@@ -64,19 +74,24 @@ for i in range(dim):
 print("];", file=ofile)
 ofile.close()
 
-for i in range(1 << dim):
-	all_path.append([labels[j] for j in range(dim) if (i & (1 << j))])
+if with_subtours:
+    print("Generating subtours...")
 
-ofile = open("../data/" + sys.argv[1] + "_subtours.dat", "w")
-print("Subtours = {", file=ofile)
+    for i in range(1 << dim):
+        all_path.append([labels[j] for j in range(dim) if (i & (1 << j))])
 
-for i in range(1,len(all_path)-1):
-	ls = ""
+    ofile = open("../data/" + sys.argv[1] + "_subtours.dat", "w")
+    print("Subtours = {", file=ofile)
 
-	for pt in all_path[i]:
-		ls += " " + str(pt)
+    for i in range(1,len(all_path)-1):
+        ls = ""
 
-	print("  <  " + str(len(all_path[i])) + "   {" + ls + "} >", file=ofile)
+        for pt in all_path[i]:
+            ls += " " + str(pt)
 
-print("};", file=ofile)
-ofile.close()
+        print("  <  " + str(len(all_path[i])) + "   {" + ls + "} >", file=ofile)
+
+    print("};", file=ofile)
+    ofile.close()
+
+print("Done")
